3e65131f41650741ff40ca04862718e15abf324c
Juan Pedro Ortega Alguacil <68635318+JuanPedroOrtega@users.noreply.github.com>
2020-12-10 15:00:25 +0100

    Chat app

---
+++ src/components/contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './contact.css'
+
+class Contact extends Component{
+    render(){
+        return(
+            <div>
+                <h1>TEST</h1>
+            </div>
+
+
+        );
+    }
+}
+
+export default Contact;
+
